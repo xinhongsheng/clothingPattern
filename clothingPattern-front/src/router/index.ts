cb1199342a6869cd64fc7d9ac889d5904a48ee7b
xinhongsheng <https://gitee.com/xinhongsheng>
2025-12-30 15:42:08 +0800

    删除PatternGen

--- clothingPattern-front/src/router/index.ts
+++ clothingPattern-front/src/router/index.ts
@@ -48,11 +48,6 @@ const router = createRouter({
       name: '用户管理',
       component: UserManagePage,
     },
-    // {
-    //   path: '/patternGeneration',
-    //   name: '智能创作',
-    //   component: PatternGenerationPage,
-    // },
     {
       path: '/mj/generation',
       name: '图案创作',
